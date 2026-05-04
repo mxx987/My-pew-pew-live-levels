3feb5cd12f85249ed2355101312b066b8d150435
mxx987 <greenx987@gmail.com>
2026-05-04 13:24:59 +0300

    Add files via upload

---
+++ Crogs/sounds/death_sounds.lua
@@ -0,0 +1,57 @@
+sounds = {
+  {
+    frequency = 445,
+    decay = 0.07,
+    frequencySweep = 1500,
+    waveform = "sine",
+  },
+  {
+    frequency = 445,
+    decay = 0.6,
+    frequencySweep = 1500,
+    waveform = "sine",
+   },
+   {
+    frequency = 550,
+    decay = 0.10,
+    vibratoDepth = 50,
+    waveform = "sawtooth",
+    },
+    {
+    frequency = 550,
+    decay = 0.8,
+    vibratoDepth = 50,
+    waveform = "sawtooth",
+    },
+   {
+    frequency = 696,
+    decay = 0.36,
+    waveform = "triangle",
+    },
+    {
+    frequency = 75,
+    decay = 0.06,
+    frequencySweep = 1700,
+    waveform = "sine",
+    },
+    {
+    frequency = 450,
+    decay = 0.6,
+    sustain = 0.5,
+    waveform = "sine",
+    },
+    {
+    frequency = 500,
+    decay = 0.1,
+    vibratoDepth = 80,
+    vibratoFrequency = 110,
+    waveform = "breaker",
+    },
+    {
+    frequency = 500,
+    decay = 0.35,
+    vibratoDepth = 80,
+    vibratoFrequency = 110,
+    waveform = "breaker",
+  }
+}
---
+++ Crogs/sounds/other_sounds.lua
@@ -0,0 +1,21 @@
+sounds = {
+  {
+    frequency = 50,
+    decay = 0.1,
+    frequencySweep = 300,
+    waveform = "sawtooth",
+  },
+  {
+  frequency = 1500,
+    decay = 1,
+    frequencyDeltaSweep = 1500,
+    vibratoDepth = 650,
+    waveform = "sine",
+  },
+  {
+    frequency = 50,
+    decay = 1,
+    frequencySweep = 300,
+    waveform = "sawtooth",
+  }
+}
