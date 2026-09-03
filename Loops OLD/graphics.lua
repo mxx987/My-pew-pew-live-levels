77a66160e84bdf0eb7672e477861daf80d35f35b
Green Stick <greenx987@gmail.com>
2026-09-03 14:06:07 +0300

    Add files via upload

---
+++ Loops OLD/graphics.lua
@@ -0,0 +1,22 @@
+meshes = {
+  {
+    vertexes = {{-500,-500}, {500,-500}, {500,500}, {-500,500}},
+    colors = {0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff},
+    segments = {{0,1,2,3,0}}
+  },
+  {
+    vertexes = {{-550,-550}, {550,-550}, {550,550}, {-550,550}},
+    colors = {0xffffffbf, 0xffffffbf, 0xffffffbf, 0xffffffbf},
+    segments = {{0,1,2,3,0}}
+  },
+  {
+    vertexes = {{-600,-600}, {600,-600}, {600,600}, {-600,600}},
+    colors = {0xffffff80, 0xffffff80, 0xffffff80, 0xffffff80},
+    segments = {{0,1,2,3,0}}
+  },
+  {
+    vertexes = {{-650,-650}, {650,-650}, {650,650}, {-650,650}},
+    colors = {0xffffff40, 0xffffff40, 0xffffff40, 0xffffff40},
+    segments = {{0,1,2,3,0}}
+  }
+}
